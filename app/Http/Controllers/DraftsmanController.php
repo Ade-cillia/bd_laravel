98e3599b387dab949b36398f76af472b823b9d03
Aurélien <aureliendu3809@outlook.fr>
2021-03-12 16:37:25 +0100

    readme

--- app/Http/Controllers/DraftsmanController.php
+++ app/Http/Controllers/DraftsmanController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Draftsman;
+use App\Models\Character;
 
 class DraftsmanController extends Controller
 {
     public static function delete($id){
+        $characters = Character::where("draftsman_id", "1")->get();
+        foreach($characters as $character){
+            $character->draftsman_id = 0;
+            $character->save();
+        }
         Draftsman::destroy($id);
         return redirect('/draftsmanList');
     }
